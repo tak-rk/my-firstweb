7ea539de87f72b083d0d60782bc6d65961f81759
高井陸斗 <takairikuto@takairikutonoMacBook-Air.local>
2022-12-14 18:06:10 +0900

    first-commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: [
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui",'tw-elements/dist/plugin'),
+  ],
+}
