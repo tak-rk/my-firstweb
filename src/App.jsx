66cbc5f48f27d0f3337983c3597985668ceba9c1
高井陸斗 <takairikuto@takairikutonoMacBook-Air.local>
2023-01-07 15:58:22 +0900

    daisyuisec

--- src/App.jsx
+++ src/App.jsx
@@ -180,7 +180,7 @@ function App() {
 
 
           <section class="space-y-10 ">
-            <div class='text-center '>
+            <div class='text-center daisy-secondary'>
               <p class="text-[46px] font-black">形にできなかったものを</p>
               <p class="text-[46px] font-black">形に致します</p> 
             </div>
