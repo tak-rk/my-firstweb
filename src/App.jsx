cb90ceac12e584cbbb73a752d7370971b74c8931
高井陸斗 <takairikuto@takairikutonoMacBook-Air.local>
2023-01-07 16:17:46 +0900

    bgc

--- src/App.jsx
+++ src/App.jsx
@@ -180,7 +180,7 @@ function App() {
 
 
           <section class="space-y-10 ">
-            <div class='text-center bg-secondary-focus'>
+            <div class='text-center bg-secondary-content'>
               <p class="text-[46px] font-black">形にできなかったものを</p>
               <p class="text-[46px] font-black">形に致します</p>            
             </div>
