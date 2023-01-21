50c1f44353b8a6cbf5d6ccac9473b71d33cf8441
高井陸斗 <takairikuto@takairikutonoMacBook-Air.local>
2023-01-21 15:21:41 +0900

    smh

--- src/App.jsx
+++ src/App.jsx
@@ -223,7 +223,7 @@ function App() {
             </footer>
             <progress className="progress w-56"></progress>
             
-            <div className="hidden:sm">
+            <div class="hidden:sm">
               {/* component here */}
               <div className="btm-nav">
                 <button>
